f124b0d5e550abd1e272989a392cedcb5b11b750
Ren Simmons <rensimmons@Rens-MacBook-Pro.local>
2016-11-03 00:32:20 -0500

    fix game

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,6 +20,15 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-
+  app.import('vendor/js/jquery/jquery-1.11.3.min.js');
+  app.import('vendor/js/jquery/jquery-migrate-1.2.1.min.js');
+  app.import('vendor/js/jquery/jquery-ui.min.js');
+  app.import('vendor/js/jquery/jquery.imgpreloader.js');
+  app.import('vendor/js/jquery/jquery.SPServices-2014.01.min.js');
+  app.import('vendor/js/jquery/jquery.ui.touch-punch.min.js');
+  app.import('vendor/js/bw/database_connection.js');
+  app.import('vendor/js/bw/document.js');
+  app.import('vendor/js/bw/game.js');
+  app.import('vendor/js/bw/list_connection.js');
   return app.toTree();
 };
