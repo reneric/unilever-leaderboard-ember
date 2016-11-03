02503a7688c177f959cf8b4cb0e040e2d7f0c181
Ren Simmons <rensimmons@Rens-MacBook-Pro.local>
2016-11-03 13:52:32 -0500

    loading styles

--- app/initializers/clock.js
+++
@@ -1,8 +0,0 @@
-import ClockService from '../services/clock';
-export default {
-  name: 'clockServiceInitializer',
-  initialize(container, application) {
-    container.register('clock:service', ClockService);
-    application.inject('controller:interval', 'clock', 'clock:service');
-  }
-};
