286809916b3aa60623d23fb5e3283afde1bbdb8a
Ren Simmons <rensimmons@Rens-MacBook-Pro.local>
2016-11-03 11:43:25 -0500

    add autoload

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  entries: Ember.computed.alias('model')
+  entries: Ember.computed.alias('model'),
+  poll: Ember.observer('entries',
+    function() {
+      Ember.run.later(this, () => {
+        console.log('reloaded');
+        this.send('reloadLeaderboard');
+      }, 10000);
+    }
+  ).on('init')
 });
--- app/routes/home.js
+++ app/routes/home.js
@@ -5,4 +5,9 @@ export default Ember.Route.extend({
   model() {
     return this.repo.getEntries({direction: 'asc'});
   },
+  actions: {
+    reloadLeaderboard() {
+      this.refresh();
+    }
+  }
 });
