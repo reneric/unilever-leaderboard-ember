27112c4e6712b4e77f91bfd9e1d84c9a1e54cfb9
Ren Simmons <rensimmons@Rens-MacBook-Pro.local>
2016-11-04 10:06:34 -0500

    add babel polypill

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     storeConfigInMeta: false,
+    babel: {
+      includePolyfill: true
+    },
   });
 
 
